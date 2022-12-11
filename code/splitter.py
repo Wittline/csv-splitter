0bcb9c2f01b3fbcd5e2901e07145ec3593ba2703
Wittline <8701464+Wittline@users.noreply.github.com>
2022-12-11 13:29:47 -0600

    Updating splitter

--- code/splitter.py
+++ code/splitter.py
@@ -1,36 +1,56 @@
-import csv
 import os
 import sys
+import gzip
 
-def split_csv(sfp, dest, prefix, size_chunk):
-    if size_chunk <= 0:
-        return
-        
-    with open(sfp, 'r') as s:
-        reader = csv.reader(s)
-        headers = next(reader)
-        fn = 0
-        exist = True
+class CSVSplit:
 
-        while exist:
-            i = 0
-            t_filename = f'{prefix}_{fn}.csv'
-            t_filepath = os.path.join(dest, t_filename)
-            print(t_filepath)
-            with open(t_filepath, 'w', newline='') as target:                
-                writer = csv.writer(target)
-                while i < size_chunk:
-                    if i == 0:
-                        writer.writerow(headers)
-                    try:
-                        writer.writerow(next(reader))
-                        i += 1
-                    except:
-                        exist = False
-                        break
-                return
-            if i == 0:
-                os.remove(t_filepath)
-            fn += 1
-  
-split_csv('Iowa_Liquor_Sales.csv', 'chunks/', 'Iowa_Liquor_Sales_', 1000)
+    def __init__(self, filepath, prefix, folder, header = True):
+        self.filepath = filepath
+        self.header = header
+        self.prefix = prefix
+        self.folder = folder
+
+    def split_process(self, filepath, chunk_size = 10):
+        chunk = []
+        with open(filepath) as f:
+
+            if self.header:
+                header = next(f)
+                chunk.append(header)
+
+            for line in f:
+                chunk.append(line)
+                if len(chunk) == chunk_size:
+                    yield chunk
+
+                    if self.header:
+                        chunk = [header]
+                    else:
+                        chunk = []
+        if chunk:
+            yield chunk 
+    
+    def split(self, chunk_size = 10, compress = False):
+
+        chunk_file = 0              
+        for chunk in self.split_process(self.filepath, chunk_size):
+
+            format = 'csv'
+
+            if compress:
+                format = 'csv.gz'
+
+            t_filename = f'{self.prefix}_{chunk_file}.{format}'
+            t_filepath = os.path.join(self.folder, t_filename)
+
+            if compress:
+                chunk_bytes = gzip.compress('\n'.join(chunk))
+                with open(t_filepath, 'wb') as chunk_file:
+                    chunk_file.write(chunk_bytes)
+            else:
+                with open(t_filepath, 'w') as file:
+                    file.writelines(chunk)
+
+            chunk_file += 1
+
+
